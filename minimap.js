66a45cbdde0905188d45bdec4dc71cc9d3b95a6d
gnzxs6bwr6-lang <gnzxs6bwr6@privaterelay.appleid.com>
2025-12-27 07:39:42 -0700

    minimap.js

--- minimap.js
+++ minimap.js
@@ -1,6 +1,9 @@
-import { player } from './Game.js';
-
-export function updateMinimap(player){
-  const mini=document.getElementById('minimap');
-  mini.innerHTML=`<div style="position:absolute;left:${75+player.position.x/2}px;top:${75+player.position.z/2}px;width:5px;height:5px;background:red;"></div>`;
+export function updateMinimap(player) {
+  const mini = document.getElementById('minimap');
+  if (!mini) return;
+  
+  const x = 75 + (player.position.x / 2);
+  const z = 75 + (player.position.z / 2);
+  
+  mini.innerHTML = `<div style="position:absolute;left:${x}px;top:${z}px;width:5px;height:5px;background:red;border-radius:50%;"></div>`;
 }
