8a384b63446f091767cb0b9cf60c2eee2e8484b6
gnzxs6bwr6-lang <gnzxs6bwr6@privaterelay.appleid.com>
2025-12-27 06:36:26 -0700

    Update and rename Game.js.txt to items.js

---
+++ items.js
@@ -0,0 +1,11 @@
+export const weapons={
+  rifle:{damage:10,rate:0.3,mag:30},
+  shotgun:{damage:25,rate:1,mag:8},
+  pistol:{damage:5,rate:0.5,mag:15}
+};
+
+export const lootItems=[
+  {name:'ammo',amount:10},
+  {name:'medkit',amount:25},
+  {name:'grenade',amount:1}
+];
