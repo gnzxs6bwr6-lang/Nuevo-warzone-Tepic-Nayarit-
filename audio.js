0c0f5dd5e63eed0bef24662da90994a9c69462e9
gnzxs6bwr6-lang <gnzxs6bwr6@privaterelay.appleid.com>
2025-12-27 06:34:10 -0700

    audio.js

--- audio.js
+++ audio.js
@@ -1,2 +1,16 @@
-const shootSound = new Audio("https://actions.google.com/sounds/v1/weapons/medium_gunshot.ogg");
-const reloadSound = new Audio("https://actions.google.com/sounds/v1/doors/door_lock.ogg");
+let sounds={};
+
+export function initAudio(){
+  ['shoot','reload','grenade'].forEach(name=>{
+    const audio=new Audio(`https://rawcdn.githack.com/KenneyNL/3D-Assets/main/${name}.mp3`);
+    audio.load();
+    sounds[name]=audio;
+  });
+}
+
+export function playSound(name){
+  if(sounds[name]){
+    sounds[name].currentTime=0;
+    sounds[name].play().catch(()=>{});
+  }
+}
